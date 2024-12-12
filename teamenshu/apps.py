1044d1646311560eba522c0c0fcf37da82b737db
Buchi63975 <s1f102302405@iniad.org>
2024-12-12 17:24:44 +0900

    apps.py修正

--- teamenshu/apps.py
+++ teamenshu/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TnsappConfig(AppConfig):
+class TeamenshuConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "Teamenshuapp"
