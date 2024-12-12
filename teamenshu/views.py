0de0b9c23d6c87112cf8d230545f842303ca244a
Buchi63975 <s1f102302405@iniad.org>
2024-12-12 17:13:01 +0900

    teamenshu/views.py制作

--- teamenshu/views.py
+++ teamenshu/views.py
@@ -1,122 +1,192 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
 from django.views import View
-from django.shortcuts import redirect
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
+
 from .models import Post, Connection
 
-# Home page displaying posts from users other than the current user
+
 class Home(LoginRequiredMixin, ListView):
+    """HOMEページで、自分以外のユーザー投稿をリスト表示"""
+
     model = Post
-    template_name = 'list.html'
+    template_name = "list.html"
 
     def get_queryset(self):
+        """リクエストユーザーのみ除外"""
         return Post.objects.exclude(user=self.request.user)
 
-# Form for creating a new post
-class CreatePost(LoginRequiredMixin, CreateView):
-    model = Post
-    template_name = 'create.html'
-    fields = ['title', 'content']
-    success_url = reverse_lazy('mypost')
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        # get_or_createにしないとサインアップ時オブジェクトがないためエラーになる
+        context["connection"] = Connection.objects.get_or_create(user=self.request.user)
+        return context
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
 
-# Display posts created by the current user
 class MyPost(LoginRequiredMixin, ListView):
+    """自分の投稿のみ表示"""
+
     model = Post
-    template_name = 'list.html'
+    template_name = "list.html"
 
     def get_queryset(self):
         return Post.objects.filter(user=self.request.user)
 
-# Detailed view of a post
+
+class CreatePost(LoginRequiredMixin, CreateView):
+    """投稿フォーム"""
+
+    model = Post
+    template_name = "create.html"
+    fields = ["title", "content"]
+    success_url = reverse_lazy("mypost")
+
+    def form_valid(self, form):
+        """投稿ユーザーをリクエストユーザーと紐付け"""
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class DetailPost(LoginRequiredMixin, DetailView):
+    """投稿詳細ページ"""
+
     model = Post
-    template_name = 'detail.html'
-
-# Edit a post (only accessible to the owner of the post)
-class UpdatePost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    template_name = 'update.html'
-    fields = ['title', 'content']
-
-    def get_success_url(self, **kwargs):
-        return reverse_lazy('detail', kwargs={"pk": self.kwargs["pk"]})
-
-    def test_func(self, **kwargs):
-        post = Post.objects.get(pk=self.kwargs["pk"])
-        return post.user == self.request.user
-
-# Delete a post (only accessible to the owner of the post)
-class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'delete.html'
-    success_url = reverse_lazy('mypost')
-
-    def test_func(self, **kwargs):
-        post = Post.objects.get(pk=self.kwargs["pk"])
-        return post.user == self.request.user
-
-# Base class for handling likes
-class LikeBase(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=self.kwargs['pk'])
-        if request.user in post.like.all():
-            post.like.remove(request.user)
-        else:
-            post.like.add(request.user)
-        return post
-
-# Like a post from the home page
-class LikeHome(LikeBase):
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-        return redirect('home')
-
-# Like a post from the detail page
-class LikeDetail(LikeBase):
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-        return redirect('detail', pk=self.kwargs['pk'])
-
-# Base class for handling follow functionality
-class FollowBase(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        target_user = Post.objects.get(pk=self.kwargs['pk']).user
-        my_connection, _ = Connection.objects.get_or_create(user=request.user)
-
-        if target_user in my_connection.following.all():
-            my_connection.following.remove(target_user)
-        else:
-            my_connection.following.add(target_user)
-        return my_connection
-
-# Follow a user from the home page
-class FollowHome(FollowBase):
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-        return redirect('home')
-
-# Follow a user from the detail page
-class FollowDetail(FollowBase):
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-        return redirect('detail', pk=self.kwargs['pk'])
-
-# Display posts from users followed by the current user
-class FollowList(LoginRequiredMixin, ListView):
-    model = Post
-    template_name = 'list.html'
-
-    def get_queryset(self):
-        my_connection, _ = Connection.objects.get_or_create(user=self.request.user)
-        return Post.objects.filter(user__in=my_connection.following.all())
+    template_name = "detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['connection'] = Connection.objects.get_or_create(user=self.request.user)[0]
+        context["connection"] = Connection.objects.get_or_create(user=self.request.user)
+        return context
+
+
+class UpdatePost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """投稿編集ページ"""
+
+    model = Post
+    template_name = "update.html"
+    fields = ["title", "content"]
+
+    def get_success_url(self, **kwargs):
+        """編集完了後の遷移先"""
+        pk = self.kwargs["pk"]
+        return reverse_lazy("detail", kwargs={"pk": pk})
+
+    def test_func(self, **kwargs):
+        """アクセスできるユーザーを制限"""
+        pk = self.kwargs["pk"]
+        post = Post.objects.get(pk=pk)
+        return post.user == self.request.user
+
+
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """投稿編集ページ"""
+
+    model = Post
+    template_name = "delete.html"
+    success_url = reverse_lazy("mypost")
+
+    def test_func(self, **kwargs):
+        """アクセスできるユーザーを制限"""
+        pk = self.kwargs["pk"]
+        post = Post.objects.get(pk=pk)
+        return post.user == self.request.user
+
+
+###############################################################
+# いいね処理
+class LikeBase(LoginRequiredMixin, View):
+    """いいねのベース。リダイレクト先を以降で継承先で設定"""
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
+        related_post = Post.objects.get(pk=pk)
+
+        if self.request.user in related_post.like.all():
+            obj = related_post.like.remove(self.request.user)
+        else:
+            obj = related_post.like.add(self.request.user)
+        return obj
+
+
+class LikeHome(LikeBase):
+    """HOMEページでいいねした場合"""
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        return redirect("home")
+
+
+class LikeDetail(LikeBase):
+    """詳細ページでいいねした場合"""
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        pk = self.kwargs["pk"]
+        return redirect("detail", pk)
+
+
+###############################################################
+
+
+###############################################################
+# フォロー処理
+class FollowBase(LoginRequiredMixin, View):
+    """フォローのベース。リダイレクト先を以降で継承先で設定"""
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
+        target_user = Post.objects.get(pk=pk).user
+
+        my_connection = Connection.objects.get_or_create(user=self.request.user)
+
+        if target_user in my_connection[0].following.all():
+            obj = my_connection[0].following.remove(target_user)
+        else:
+            obj = my_connection[0].following.add(target_user)
+        return obj
+
+
+class FollowHome(FollowBase):
+    """HOMEページでフォローした場合"""
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        return redirect("home")
+
+
+class FollowDetail(FollowBase):
+    """詳細ページでフォローした場合"""
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        pk = self.kwargs["pk"]
+        return redirect("detail", pk)
+
+
+###############################################################
+
+
+class FollowList(LoginRequiredMixin, ListView):
+    """フォローしたユーザーの投稿をリスト表示"""
+
+    model = Post
+    template_name = "list.html"
+
+    def get_queryset(self):
+        """フォローリスト内にユーザーが含まれている場合のみクエリセット返す"""
+        my_connection = Connection.objects.get_or_create(user=self.request.user)
+        all_follow = my_connection[0].following.all()
+        return Post.objects.filter(user__in=all_follow)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["connection"] = Connection.objects.get_or_create(user=self.request.user)
         return context
