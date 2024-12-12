2fa3d86228f56403a513412f6ae804a9c8dce38c
Buchi63975 <s1f102302405@iniad.org>
2024-12-12 17:01:06 +0900

    修正

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("accounts/", include("allauth.urls")),
-        path("", include("snsapp.urls")),
+        path("", include("teamenshu.urls")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
