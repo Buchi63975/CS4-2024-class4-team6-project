4f6cedc7bef79d5e5b9a6980a88e7d22a1eeb7b7
1F10230067 <136304730+1F10230067@users.noreply.github.com>
2024-12-12 16:55:21 +0900

    初期設定

--- teamenshu/apps.py
+++ teamenshu/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TeamenshuConfig(AppConfig):
+class SnsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teamenshu'
+    name = 'teamenshuapp'
